1d62485b3aafa5229f0111894f669bffeddd2b29
Justin Blank <justin.blank@gmail.com>
2021-11-06 21:11:28 -0400

    Parsing code

--- ramhog.c
+++ ramhog.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 uint64_t nonce = 14192476102350395398ULL; // chosen randomly
@@ -19,6 +20,72 @@ uint64_t next_random() {
 }
 
 int parse_options(struct options *opt, int argc, char **argv) {
+  size_t size;
+  long touch_interval = 0;
+  _Bool should_touch = 0;
+  _Bool touch_randomly = 0;
+  if (argc < 1) {
+    return -1;
+  }
+
+  for (int i = 0;; i++) {
+    char c = argv[1][i];
+    if (c < '0' || c > '9') {
+      if (c == 'K' || c == 'M' || c == 'G') {
+	errno = 0;
+	uint64_t passed_size = strtoull(argv[1], NULL, 10);
+	if (errno) {
+	  fprintf(stderr, "Parse error for memory size %s. Aborting.\n", argv[1]);
+	  return errno;
+	}
+	if (c == 'K') {
+	  passed_size *= 1024;
+	}
+	else if (c == 'M') {
+	  passed_size *= 1024ULL * 1024ULL;
+	}
+	else if (c == 'G') {
+	  passed_size *= 1024ULL * 1024ULL * 1024ULL;
+	}
+	if (passed_size > SIZE_MAX) {
+	  fprintf(stderr, "Size %lu too large", passed_size);
+	  return -1;
+	}
+	size = passed_size;
+	break;
+      }
+      else {
+	fprintf(stderr, "Illegal first argument: %s. Aborting.\n", argv[1]);
+	return -1;
+      }
+    }
+  }
+  for (int i = 2; i < argc; i++) {
+    if (!strncmp(argv[i], "--touch", 7)) {
+      should_touch = 1;
+    }
+    else if (!strncmp(argv[i], "--random", 8)) {
+      touch_randomly = 1;
+    }
+    else if (!strncmp(argv[i], "--sleep=", 8)) {
+      char *tail = &argv[i][8];
+      errno = 0;
+      touch_interval = strtoull(tail, NULL, 10);
+      if (errno) {
+	fprintf(stderr, "Invalid sleep option %s. Aborting.\n", argv[i]);
+	return errno;
+      }
+    }
+    else {
+      fprintf(stderr, "Unrecognized option %s. Aborting", argv[i]);
+    }
+  }
+
+  opt->size = size;
+  opt->touch_interval = touch_interval;
+  opt->should_touch = should_touch;
+  opt->touch_randomly = touch_randomly;
+
   return 0;
 }
 
@@ -74,15 +141,13 @@ uint64_t run(struct options *program_options) {
 
 int main(int argc, char **argv) {
   struct options program_options;
-  program_options.size = 65556ULL * 65556;
-  program_options.touch_interval = 1000000;
-  program_options.should_touch = 1;
-  program_options.touch_randomly = 1;
-  uint64_t result = run(&program_options);
-  // avoid dead code elimination
-  if (result == UINT64_MAX) {
-    printf("This should probably never happen.");
-  }
-  return 0;
-}
+  int parsed = parse_options(&program_options, argc, argv);
 
+  if (parsed == 0) {
+    uint64_t result = run(&program_options);
+    if (result == 5) {
+      printf("Surprisingly a five!\n"); // TODO: fix
+    }
+  }
+  return parsed;
+}
